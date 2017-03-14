7b85897043366876c2d763933b2cfbc5df59c0d0
Maurizio Manetti <maurizio@imanetti.net>
2017-03-14 17:35:44 +0100

    Added pandoc to convert README.md to reText format

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+import os
+
+long_description = """HTTP server and request handler built on top of BaseHTTPServer 
+intended for Raspberry Pi projects with a web interface"""
+
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
 try:
   from setuptools import setup
 except ImportError:
@@ -8,9 +15,9 @@ setup(
   name='RPiHTTPServer',
   author='Maurizio Manetti',
   author_email='maurizio@imanetti.net',
-  version='0.0.1a',
+  version='0.0.1',
   url='http://github.com/mauntrelio/RPiHTTPServer',
-  description='HTTP server eand request handler '
+  description='HTTP server and request handler '
               'built on top of BaseHTTPServer '
               'intended for Raspberry Pi projects '
               'with a web interface',
